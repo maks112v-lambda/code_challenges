b2f6cb92e39b214fd57b35ed11632816a7c3c71d
Maksim <maks112v@gmail.com>
2019-05-13 11:44:24 -0400

    added new challenge

---
+++ 2 - Easy/2 - Largest and Smallest.js
@@ -0,0 +1,21 @@
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+// minMax([2334454, 5]) ➞ [5, 2334454]
+
+// minMax([1]) ➞ [1, 1]
+// Notes
+// All test arrays will have at least one element and are valid.
+
+const minMax = nums => {
+  nums.sort()
+  return [nums[0], nums[nums.length - 1]]
+}
+
+console.log(minMax([1, 2, 3, 4, 5]))
+
+console.log(minMax([2334454, 5]))
+
+console.log(minMax([1]))
