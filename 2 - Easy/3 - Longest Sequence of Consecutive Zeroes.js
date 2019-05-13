6835617fe76e1b67f0cc064f5d99d457adf66324
Maksim <maks112v@gmail.com>
2019-05-13 11:54:20 -0400

    Update 3 - Longest Sequence of Consecutive Zeroes.js

---
+++ 2 - Easy/3 - Longest Sequence of Consecutive Zeroes.js
@@ -0,0 +1,26 @@
+// Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+
+// Examples
+// longestZero("01100001011000") ➞ "0000"
+
+// longestZero("100100100") ➞ "00"
+
+// longestZero("11111") ➞ ""
+// Notes
+// If no zeroes exist in the input, return an empty string.
+
+// Directions:
+// 1. Complete MVP
+// 2. Turn into an arrow function
+// 3. Add Extra Tests
+
+const longestZero = numbers => {
+  numbers = numbers.split('1').sort().filter(Boolean)
+  return numbers[numbers.length - 1] ? numbers[numbers.length - 1] : ''
+}
+
+console.log(longestZero('01100001011000'))
+
+console.log(longestZero('100100100'))
+
+console.log(longestZero('11111'))
