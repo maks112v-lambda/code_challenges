9237dd300308d2d76a635896a296983ab2b64bd5
Maksim <maks112v@gmail.com>
2019-05-15 12:04:03 -0400

    added code challenge solution

--- 2 - Easy/3 - Longest Sequence of Consecutive Zeroes.js
+++ 2 - Easy/3 - Longest Sequence of Consecutive Zeroes.js
@@ -5,7 +5,7 @@
 
 // longestZero("100100100") ➞ "00"
 
-// longestZero("11111") ➞ ""
+// longestZero("11111") ➞ "none"
 // Notes
 // If no zeroes exist in the input, return an empty string.
 
@@ -14,9 +14,11 @@
 // 2. Turn into an arrow function
 // 3. Add Extra Tests
 
+var start = new Date();
+
 const longestZero = numbers => {
   numbers = numbers.split('1').sort().filter(Boolean)
-  return numbers[numbers.length - 1] ? numbers[numbers.length - 1] : ''
+  return numbers[numbers.length - 1] ? numbers[numbers.length - 1] : '';
 }
 
 console.log(longestZero('01100001011000'))
@@ -24,3 +26,7 @@ console.log(longestZero('01100001011000'))
 console.log(longestZero('100100100'))
 
 console.log(longestZero('11111'))
+
+var end = new Date(); 
+var diff = end - start;
+console.log(`Code ran in ${diff} ms`)
