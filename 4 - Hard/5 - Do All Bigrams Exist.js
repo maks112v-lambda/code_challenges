2be4d7cc28ddf20ef73a03d0cb4e7aadffae35b6
Maksim V <maks112v@gmail.com>
2019-06-13 08:31:22 -0400

    Update 5 - Do All Bigrams Exist.js

--- 4 - Hard/5 - Do All Bigrams Exist.js
+++ 4 - Hard/5 - Do All Bigrams Exist.js
@@ -14,10 +14,25 @@
 // Notes
 // A bigram is string of two consecutive characters.
 
+const canFind = (bigrams, words) => {
+  let check = true;
+  const string = words.join('-');
+  bigrams.forEach(el => {
+    if (!string.includes(el)) {
+      check = false;
+    }
+  });
+  return check;
+};
+
 console.log(canFind(['at', 'be', 'th', 'au'], ['beautiful', 'the', 'hat']));
 
 console.log(
   canFind(['ay', 'be', 'ta', 'cu'], ['maybe', 'beta', 'abet', 'course']),
+);
+
+console.log(
+  canFind(['th', 'fo', 'ma', 'or'], ['the', 'many', 'for', 'forest']),
 );
 
 console.log(canFind(['oo', 'mi', 'ki', 'la'], ['milk', 'chocolate', 'cooks']));
